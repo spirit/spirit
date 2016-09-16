2efb1b3df7bd1fff1b5154d6171e5f7f3255c642
Patrick Brouwer <patrick@inlet.nl>
2016-09-16 14:17:58 +0200

    Setup tests
    + Wallaby
    + Chai + promised
    + Sinon
    + Babel

---
+++ wallaby.config.js
@@ -0,0 +1,41 @@
+'use strict'
+
+process.env.BABEL_ENV = 'test'
+
+module.exports = function(wallaby) {
+
+  return {
+
+    files: [
+      { pattern: 'src/**/*.js', load: false },
+      { pattern: 'test/fixtures/*.js', instrument: false },
+      'test/spec-setup.js'
+    ],
+
+    tests: [
+      'test/*-spec.js'
+    ],
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel()
+    },
+
+    env: {
+      type: 'node'
+    },
+
+    workers: { recycle: true },
+
+    middleware: function middleware(app, express) {
+      app.use('/test/fixtures', express.static(
+        require('path').join(__dirname, 'test/fixtures')
+      ))
+    },
+
+    setup: function setup() {
+      require('./test/spec-setup')
+    }
+
+  }
+
+}
