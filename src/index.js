75e6c49ab3b98e21cc08b59865d46e8ea5f6e94b
Patrick Brouwer <patrick@inlet.nl>
2016-09-23 13:02:15 +0200

    Use relative paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import config from 'config/config'
-import setup from 'config/setup'
-import { parse, load } from 'data/parser'
+import config from './config/config'
+import setup from './config/setup'
+import { parse, load } from './data/parser'
 import group from './group/group'
 
 export {
