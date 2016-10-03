0deb673d121b49c043724f105086926a3e787249
Patrick Brouwer <patrick@inlet.nl>
2016-10-03 16:24:35 +0200

    Add transitions

--- src/group/transitions.js
+++ src/group/transitions.js
@@ -1,5 +1,20 @@
-class Transitions {
+import List from '../list/list'
+import Transition from './transition'
 
+class Transitions extends List {
+
+  constructor(transitions) {
+    super(transitions, Transition, [0])
+    this.duplicates = { prop: 'frame' }
+  }
+
+  /**
+   * Get a transition by frame
+   * @param {number} frame
+   */
+  get(frame) {
+    return this._list.find(p => p.frame === frame)
+  }
 }
 
 export default Transitions
---
+++ test/transitions-spec.js
@@ -0,0 +1,62 @@
+import Transitions from '../src/group/transitions'
+import Transition from '../src/group/transition'
+
+describe('transitions', () => {
+
+  it('should have empty transitions', () => {
+    expect(new Transitions()).to.have.lengthOf(0)
+  })
+
+  it('should parse array of transitions by objects', () => {
+    const trs = new Transitions([{ frame: 0 }, { frame: 12 }, { frame: 49 }])
+    expect(trs).to.have.lengthOf(3)
+    expect(trs.at(0).toObject(true)).to.deep.equal({ frame: 0, ease: 'Linear.easeNone', params: [] })
+    expect(trs.at(1).toObject(true)).to.deep.equal({ frame: 12, ease: 'Linear.easeNone', params: [] })
+    expect(trs.at(2).toObject(true)).to.deep.equal({ frame: 49, ease: 'Linear.easeNone', params: [] })
+  })
+
+  describe('#toArray', () => {
+
+    let trs
+
+    beforeEach(() => {
+      trs = new Transitions([{ frame: 0 }, { frame: 12 }, { frame: 49, params: { x: 200, y: 500 } }])
+    })
+
+    it('should convert to object', () => {
+      expect(trs.toArray()).to.deep.equal([
+        { frame: 0, ease: 'Linear.easeNone', params: {} },
+        { frame: 12, ease: 'Linear.easeNone', params: {} },
+        { frame: 49, ease: 'Linear.easeNone', params: { x: 200, y: 500 } },
+      ])
+    })
+
+    it('should convert to object with params as array', () => {
+      expect(trs.list.map(tr => tr.toObject(true))).to.deep.equal([
+        { frame: 0, ease: 'Linear.easeNone', params: [] },
+        { frame: 12, ease: 'Linear.easeNone', params: [] },
+        { frame: 49, ease: 'Linear.easeNone', params: [{ x: 200 }, { y: 500 }] },
+      ])
+    })
+  })
+
+  describe('#get', () => {
+
+    let trs
+
+    beforeEach(() => {
+      trs = new Transitions([{ frame: 0 }, { frame: 12 }, { frame: 49, params: { x: 200, y: 500 } }])
+    })
+
+    it('should get a transition by frame', () => {
+      const tr = trs.get(49)
+      expect(tr).to.be.an.instanceOf(Transition)
+      expect(tr.params.toObject()).to.deep.equal({ x: 200, y: 500 })
+    })
+
+    it('should get no transition by frame', () => {
+      expect(trs.get(4)).to.be.undefined
+      expect(trs.get(13)).to.be.undefined
+    })
+  })
+})
