9d63e5d430c1b8045b311f92cc22abaa80d3fb3c
Patrick Brouwer <patrick@inlet.nl>
2016-10-14 13:29:55 +0200

    Expose group classes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import config from './config/config'
 import setup from './config/setup'
 import { create, load } from './data/parser'
 
+import {
+  Group,
+  Groups,
+  EvalMap,
+  Param,
+  Params,
+  Timeline,
+  Timelines,
+  Transition,
+  Transitions
+} from './group'
+
 export {
   config,
   setup,
   load,
-  create
+  create,
+  Group,
+  Groups,
+  EvalMap,
+  Param,
+  Params,
+  Timeline,
+  Timelines,
+  Transition,
+  Transitions
 }
 
