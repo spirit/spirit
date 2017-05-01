93d214e0b4c243d5e483c71843ff571d85146529
Patrick Brouwer <patrick@inlet.nl>
2017-05-01 11:06:09 +0200

    Update keyframe + add tests

--- src/group/keyframe.js
+++ src/group/keyframe.js
@@ -1,25 +1,51 @@
 import { EventEmitter } from 'events'
+import { is } from '../utils'
 import { emitChange } from '../utils/emitter'
 
 /**
  * Single keyframe.
  *
- * @example { time: "0.1s", value: 10, ease: "Linear.easeNone" }
+ * @example
+ *
+ *   { '0.1s': { value: 10, ease: 'Linear.easeNone' } }
  */
 
-@emitChange('time', null, val => typeof val === 'string')
-@emitChange('value')
-@emitChange('ease')
+@emitChange('time', null, [
+  {
+    validator: val => typeof val === 'string',
+    message: 'Time must be a string'
+  },
+  {
+    validator: val => /^(?:\d+|\d+(?::|\.)\d+)(?:s|ms)$/.test(val),
+    message: 'Time signature invalid'
+  }
+])
+@emitChange('value', null)
+@emitChange('ease', null)
 
-class KeyFrame extends EventEmitter {
+class Keyframe extends EventEmitter {
 
+  /**
+   * Keyframe.
+   *
+   * @param {string}  time    position on timeline
+   * @param {*}       value   value assigned
+   * @param {string}  ease    easing value (optional)
+   */
   constructor(time, value, ease) {
     super()
     this.setMaxListeners(Infinity)
 
+    ease = ease || null
+
     Object.assign(this, { time, value, ease })
   }
 
+  /**
+   * Convert to readable object
+   *
+   * @returns {object}
+   */
   toObject() {
     return {
       [this.time]: {
@@ -30,4 +56,31 @@ class KeyFrame extends EventEmitter {
   }
 }
 
-export default KeyFrame
+/**
+ * Create keyframe instance from object
+ *
+ * @param   {object} obj { "0.2s": { value: 10, ease: "Linear.easeNone" }}
+ * @returns {Keyframe}
+ */
+Keyframe.fromObject = function(obj) {
+  if (!is.isObject(obj)) {
+    throw new Error('Object is invalid')
+  }
+
+  const keys = Object.keys(obj)
+
+  if (keys.length === 0 || keys.length > 1) {
+    throw new Error('Object is invalid')
+  }
+
+  const time = keys[0]
+  const { value, ease } = obj[time]
+
+  if (value === undefined) {
+    throw new Error('Object is invalid')
+  }
+
+  return new Keyframe(time, value, ease)
+}
+
+export default Keyframe
--- test/keyframe-spec.js
+++ test/keyframe-spec.js
@@ -1,6 +1,6 @@
 import Keyframe from '../src/group/keyframe'
 
-describe.only('keyframe', () => {
+describe('keyframe', () => {
 
   it('should create a keyframe', () => {
     const kf = new Keyframe('0.2s', 10, 'Linear.easeNone')
@@ -9,14 +9,43 @@ describe.only('keyframe', () => {
     expect(kf).to.have.property('ease', 'Linear.easeNone')
   })
 
-  it('should create an object', async () => {
+  it('should create an object', () => {
     expect(new Keyframe('1s', 20, 'Power1.easeOut').toObject()).to.deep.equal({
       '1s': { value: 20, ease: 'Power1.easeOut' }
     })
   })
 
-  it('should fail on invalid time', async() => {
-    expect(() => new Keyframe(10, 10)).to.throw(/time is invalid/)
+  it('should fail on invalid time', () => {
+    expect(() => new Keyframe(10, 10)).to.throw(/Time must be a string/)
+  })
+
+  it('should fail on invalid time signature', () => {
+    expect(() => new Keyframe('sss', 10)).to.throw(/Time signature invalid/)
+  })
+
+  describe('from object', () => {
+
+    it('should fail on parse invalid object', () => {
+      expect(() => Keyframe.fromObject({})).to.throw(/Object is invalid/)
+      expect(() => Keyframe.fromObject({ x: 123 })).to.throw(/Object is invalid/)
+      expect(() => Keyframe.fromObject({ time: 123 })).to.throw(/Object is invalid/)
+      expect(() => Keyframe.fromObject(234)).to.throw(/Object is invalid/)
+      expect(() => Keyframe.fromObject([])).to.throw(/Object is invalid/)
+    })
+
+    it('should fail on parse invalid time', () => {
+      expect(() => Keyframe.fromObject({ 123: { value: 10 } })).to.throw(/Time signature invalid/)
+      expect(() => Keyframe.fromObject({ 'invalid-time': { value: 10 } })).to.throw(/Time signature invalid/)
+    })
+
+    it('should parse valid object', () => {
+      const keyframe = Keyframe.fromObject({ '02s': { value: '+=10deg' } })
+
+      expect(keyframe).to.have.property('time', '02s')
+      expect(keyframe).to.have.property('value', '+=10deg')
+      expect(keyframe).to.have.property('ease', null)
+    })
+
   })
 
 })
