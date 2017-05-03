e7521a5daa02f8a8fd80c596705a96da02007091
Patrick Brouwer <patrick@inlet.nl>
2017-05-03 17:42:02 +0200

    Add events to props

--- src/group/props.js
+++ src/group/props.js
@@ -1,5 +1,23 @@
-class Props {
+import List from '../list/list'
+
+class Props extends List {
 
 }
+
+Props.Events = [
+  'change:list',
+  'add',
+  'remove',
+  'change',
+  'change:name',
+  'change:keyframes',
+  'change:keyframes:list',
+  'change:keyframe',
+  'change:keyframe:time',
+  'change:keyframe:value',
+  'change:keyframe:ease',
+  'add:keyframe',
+  'remove:keyframe'
+]
 
 export default Props
---
+++ test/props-spec.js
@@ -0,0 +1,8 @@
+import Props from '../src/group/props'
+import Prop from '../src/group/prop'
+import Keyframes from '../src/group/keyframes'
+import Keyframe from '../src/group/keyframe'
+
+describe.only('properties', () => {
+
+})
