e8dc731b8a121113a239c19271d4df477368b5e3
Patrick Brouwer <patrick@inlet.nl>
2017-05-05 17:55:18 +0200

    Update fixtures, remove transitions

--- test/fixtures/group/groups-data.js
+++ test/fixtures/group/groups-data.js
@@ -25,7 +25,7 @@ const params = [
  * @param label
  * @param transitions
  */
-export function timeline(id, path, label, transitions = 2) {
+export function timeline(id, path, label) {
   const tl = {}
 
   if (id) {
@@ -40,16 +40,6 @@ export function timeline(id, path, label, transitions = 2) {
     tl.label = label
   }
 
-  tl.transitions = []
-
-  for (let i = 0; i < transitions; i++) {
-    tl.transitions.push({
-      frame: parseInt(Math.random() * 600),
-      ease: random(eases),
-      params: random(params)
-    })
-  }
-
   return tl
 }
 
@@ -62,40 +52,19 @@ export const jsonGhost = JSON.stringify({
       "timelines": [
         {
           "path": "svg[1]",
-          "transitions": [
-            { "frame": 0, "ease": "Linear.easeNone", "params": { "autoAlpha": "0.995" } },
-            { "frame": 54, "ease": "Linear.easeNone", "params": { "x": 0, "y": 0 } },
-            { "frame": 66, "ease": "Back.easeInOut", "params": { "x": -2, "y": 361 } }
-          ]
+          "props": {}
         },
         {
           "path": "svg[1]/path[1]",
-          "transitions": [
-            { "frame": 0, "ease": "Linear.easeNone", "params": { "autoAlpha": "0" } },
-            { "frame": 46, "ease": "Linear.easeNone", "params": { "autoAlpha": "0" } },
-            { "frame": 50, "ease": "Linear.easeNone", "params": { "autoAlpha": "1" } }
-          ]
+          "props": {}
         },
         {
           "id": "eyes",
-          "transitions": [
-            { "frame": 0, "params": { "scaleX": "3", "scaleY": "3", "x": -16.74, "y": 2.31, "autoAlpha": "0" } },
-            { "frame": 11, "ease": "Power2.easeOut", "params": { "autoAlpha": 1 } },
-            { "frame": 15, "ease": "Linear.easeNone", "params": { "scaleY": 3.95525 } },
-            { "frame": 16, "ease": "Linear.easeNone", "params": { "scaleY": 0.1 } },
-            { "frame": 17, "ease": "Linear.easeNone", "params": { "scaleY": 3.95525 } },
-            { "frame": 29, "ease": "Linear.easeNone", "params": { "scaleY": 3.95525 } },
-            { "frame": 30, "ease": "Linear.easeNone", "params": { "scaleY": 0.1 } },
-            { "frame": 31, "ease": "Linear.easeNone", "params": { "scaleY": 3.95525 } },
-            { "frame": 34, "ease": "Linear.easeNone", "params": { "scaleY": 3.95525 } },
-            { "frame": 35, "ease": "Linear.easeNone", "params": { "scaleY": 0.1 } },
-            { "frame": 36, "ease": "Linear.easeNone", "params": { "scaleY": 3.95525 } },
-            { "frame": 53, "ease": "Elastic.easeInOut", "params": { "scaleX": 1, "scaleY": 1, "x": -5.76, "y": 1.15 } }
-          ]
+          "props": {}
         },
         {
           "path": "svg[1]/path[2]",
-          "transitions": [{ "frame": 0, "ease": "Linear.easeNone", "params": { "autoAlpha": "0" } }]
+          "props": {}
         }
       ],
     }
