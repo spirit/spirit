503cc47fd09cc4746197a42fe073197b85114f36
Patrick Brouwer <patrick@inlet.nl>
2017-05-08 13:40:25 +0200

    Export default module
    Can be used:
    
    ```
    // default import
    import spirit from 'spiritjs'
    
    // import module
    import { setup } from 'spiritjs'
    ```

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import { debug, context } from './utils'
 
 const version = require('../package.json').version
 
-export config from './config/config'
-export setup from './config/setup'
-export groups from './registry/registry'
-export { create, load } from './data/parser'
-export { version }
+import config from './config/config'
+import setup from './config/setup'
+import groups from './registry/registry'
+import { create, load } from './data/parser'
 
 if (debug) {
   console.warn('You are running the development build of Spirit.')
+
+const Spirit = function() {
+  this.config = config
+  this.version = version
+  this.setup = setup
+  this.groups = groups
+  this.create = create
+  this.load = load
+}
+
+export {
+  config,
+  version,
+  setup,
+  groups,
+  create,
+  load
+}
+
+const spirit = new Spirit()
+
+module.exports = spirit
 }
