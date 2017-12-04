8b672c3d622d7d009d5af9533393e6f4a6c3518a
Patrick Brouwer <patrick@inlet.nl>
2017-12-04 14:25:21 +0100

    2.0.11

--- dist/spirit.js
+++ dist/spirit.js
@@ -1,5 +1,5 @@
 /*!
- * Spirit.js v2.0.10
+ * Spirit.js v2.0.11
  * 
  * (c) 2017 Patrick Brouwer
  * Released under the MIT License.
@@ -2783,7 +2783,7 @@ function loadScript(src) {
 
 
 
-var version = '2.0.10';
+var version = '2.0.11';
 
 /**
  * Setup Spirit GSAP
@@ -3888,7 +3888,7 @@ module.exports = g;
 /* harmony export (binding) */ __webpack_require__.d(exports, "version", function() { return version; });
 
 
-var version = '2.0.10';
+var version = '2.0.11';
 
 
 
