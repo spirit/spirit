3a0079ffbd34dc0a1d51859a1abf8ec70d23615c
Patrick Brouwer <patrick@inlet.nl>
2017-12-13 18:11:15 +0100

    Add resolver util

---
+++ src/utils/resolver.js
@@ -0,0 +1,36 @@
+import debug from './debug'
+import * as xpath from './xpath'
+
+/**
+ * Resolve element relative to root
+ *
+ * @param {Element}       root
+ * @param {{ path, id }}  data
+ * @param {boolean}       throwException
+ */
+export function resolveElement(root, data, throwException = false) {
+  let transformObject = null
+  let { path, id } = data
+
+  if (id) {
+    transformObject = root.querySelector(`[data-spirit-id="${id}"]`)
+  }
+
+  if (!transformObject && path) {
+    transformObject = xpath.getElement(path, root === document.body ? undefined : root)
+  }
+
+  if (!transformObject) {
+    if (debug()) {
+      console.group('Unable to resolve element')
+      console.warn('Timeline: ', data)
+      console.groupEnd()
+    }
+
+    if (throwException) {
+      throw new Error('Cannot find element.')
+    }
+  }
+
+  return transformObject
+}
