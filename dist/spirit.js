3a7068e68f15d68465ea61ff7d406eb7842141e7
Patrick Brouwer <patrick@inlet.nl>
2023-07-11 12:33:34 +0200

    3.1.13

--- dist/spirit.js
+++ dist/spirit.js
@@ -1,5 +1,5 @@
 /*!
- * Spirit.js v3.1.12
+ * Spirit.js v3.1.13
  *
  * (c) 2023 Patrick Brouwer
  * Released under the MIT License.
@@ -3645,7 +3645,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _loadAnimation__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./loadAnimation */ "./src/loadAnimation.js");
 /* harmony import */ var _utils_debug__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./utils/debug */ "./src/utils/debug.js");
 
-var version = "3.1.12";
+var version = "3.1.13";
 
 
 
